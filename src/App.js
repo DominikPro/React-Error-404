b6bc2feb092add2f0ac021fd67708b8d635bcbce
Dominik Proficz <dominik.proficz@gmail.com>
2019-09-15 00:19:19 +0200

    React router Spa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
 
+const Home = () => <h1>Strona Startowa</h1>;
+const News = () => <h1>Aktualności</h1>;
+const Contact = () => <h1>Kontakt</h1>;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/" exact activeClassName="home_selected">
+                  Start
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/news" activeClassName="news_selected">
+                  Aktualności
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" activeClassName="contact_selected">
+                  Kontakt
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <Route path="/" exact component={Home} />
+          <Route path="/news" component={News} />
+          <Route path="/contact" component={Contact} />
+        </section>
+      </div>
+    </Router>
   );
 }
 
